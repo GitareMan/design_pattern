3d51bf4c5b85b751579a40e7e7accb3bb410c2b5
Ihor Drozd <my-app@ukre.net>
2020-10-07 20:40:06 +0300

    First commit

---
+++ singleton/Singleton.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+
+class Singleton 
+{
+
+    public $data;
+
+    private function __construct()
+    {   
+        $this->data = rand();
+    }
+
+    private static $instance = null;
+
+    public static function getInstance()
+    {
+        if(is_null(self::$instance))
+        self::$instance = new self;
+
+        return self::$instance;
+    }
+
+
+    private function __clone () {}
+    private function __wakeup (){}
+
+
+}
+
+
